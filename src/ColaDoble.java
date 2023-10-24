35a0640f6443d8cd2c88e881a64d1584af66e0c3
Alonso <abrahamreynoso64@gmail.com>
2023-10-24 13:58:20 -0600

    cambios

--- src/ColaDoble.java
+++ src/ColaDoble.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ColaDoble {
 
-    final static int MAX = 10;
+    final static int MAX = 2;
     static int[] cola = new int[MAX];
     static int inicio = -1;
     static int fin = -1;
@@ -13,34 +13,40 @@ public class ColaDoble {
     public void Insertar(int valor) {
 
         Scanner sc = new Scanner(System.in);
-        if (inicio == -1) {
-            inicio = fin = 0;
-            cola[fin] = valor;
-            System.out.println("Tú elemento: " + valor + " Se inserto en la posicion: " + "[" + fin + "]");
-        } else {
-            System.out.println("¿Quieres Insertar por el final de la cola? \n 1).Si \n 2).No");
-            String opcion = sc.nextLine();
-            switch (opcion) {
-                case "1":
-                    if (fin == MAX - 1) {
-                        System.out.println("No se puede insertar por el final, la cola ya esta llena.");
-                    } else {
-                        fin = fin + 1;
-                        cola[fin] = valor;
-                        System.out.println("Tú elemento: " + valor + " Se inserto en la posicion: " + "[" + fin + "]");
-                    }
-                    break;
-                case "2":
-                    if (inicio == 0) {
-                        System.out.println("No se puede insertar por el inicio, ya hay un elemento ocupando ese lugar.");
-                    } else {
-                        inicio = inicio - 1;
-                        cola[inicio] = valor;
-                        System.out.println("Tú elemento: " + valor + " Se inserto en la posicion: " + "[" + fin + "]");
-                    }
-                    break;
-            }
 
+        if(fin == MAX - 1){
+            System.out.println("La cola ya esta llena.");
+        }
+        else{
+            if (inicio == -1) {
+                inicio = fin = 0;
+                cola[fin] = valor;
+                System.out.println("Tú elemento: " + valor + " Se inserto en la posicion: " + "[" + fin + "]");
+            } else {
+                System.out.println("¿Quieres Insertar por el final de la cola? \n 1).Si \n 2).No");
+                String opcion = sc.nextLine();
+                switch (opcion) {
+                    case "1":
+                        if (fin == MAX - 1) {
+                            System.out.println("No se puede insertar por el final, la cola ya esta llena.");
+                        } else {
+                            fin = fin + 1;
+                            cola[fin] = valor;
+                            System.out.println("Tú elemento: " + valor + " Se inserto en la posicion: " + "[" + fin + "]");
+                        }
+                        break;
+                    case "2":
+                        if (inicio == 0) {
+                            System.out.println("No se puede insertar por el inicio, ya hay un elemento ocupando ese lugar.");
+                        } else {
+                            inicio = inicio - 1;
+                            cola[inicio] = valor;
+                            System.out.println("Tú elemento: " + valor + " Se inserto en la posicion: " + "[" + fin + "]");
+                        }
+                        break;
+                }
+
+            }
         }
     }
 
@@ -66,12 +72,13 @@ public class ColaDoble {
         }
     }
 
+
     public void EliminarItem() {
         if (inicio == -1 && fin == -1) {
             System.out.println("No hay nada que borrar. La cola esta vacía.");
         } else {
 
-            System.out.println("¿Quieres insertar por el inicio o final de la cola? \n 1).Inicio \n 2).Final");
+            System.out.println("¿Quieres eliminar por el inicio o final de la cola? \n 1).Inicio \n 2).Final");
             String opcion = sc.nextLine();
 
             switch (opcion) {
@@ -101,6 +108,7 @@ public class ColaDoble {
 
         }
     }
+
 }
 //Insertar
 //BorrarCola
